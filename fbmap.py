19732f817bcc449be40be79309835431ffec5429
root <sinisterpatrician@gmail.com>
2017-02-18 14:54:41 -1000

    Fixed code style + added docs

--- fbmap.py
+++ fbmap.py
@@ -1,76 +1,100 @@
 #!/usr/bin/env python3
+'''
+This program plots fail2ban logs by geolocating IPs from
+the logfile and then plotting them on a Google Maps doc
+'''
 
-import sys
-import subprocess
 import argparse
 import os
-import requests
-import gmplot
+import subprocess
+import sys
 import webbrowser
+
 from bs4 import BeautifulSoup
+import gmplot
+import requests
 
 def is_valid_file(parser, arg):
-        if not os.path.exists(arg):
-            parser.error("The file %s does not exist!" % arg)
-        else:
-            return open(arg, 'r')
+    '''
+    Helper function that checks if log file is valid
+    and returns file object to main()
+    '''
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return open(arg, 'r')
 
 def clean_logfile(log):
-        command = "grep -iv unban {} | awk '{print $7}' | tail -n+2".format(log)
-        return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.readlines()
+    '''
+    Function uses gnu utils to turn fail2ban.log
+    to all IP addresses
+    '''
+    command = "grep -iv unban {} | awk '{print $7}' | tail -n+2".format(log)
+    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT).stdout.readlines()
 
 def map_ip(ipaddr):
-        '''
-        url = "https://ipinfo.io/%s/json" % ipaddr
-        r = requests.get(url).json()
-        print(r['city'],r['country'],r['loc'])
-        '''
-        url = "http://freegeoip.net/json/%s" % ipaddr
-        r = requests.get(url).json()
-        print(r['city'],r['country_code'],r['latitude'],r['longitude'])
-        return r['latitude'], r['longitude']
+    '''
+    url = "https://ipinfo.io/%s/json" % ipaddr
+    r = requests.get(url).json()
+    print(r['city'],r['country'],r['loc'])
+    '''
+    url = "http://freegeoip.net/json/%s" % ipaddr
+    req = requests.get(url).json()
+    print(req['city'], req['country_code'], req['latitude'], req['longitude'])
+    return req['latitude'], req['longitude']
 
 def start_browser():
-        import platform
-        system = platform.system()
-        if system == "Darwin":
-                chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-        elif system == "win32":
-                # Windows
-                chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
-        else:
-                print("Please open ./mymap.html in your favorite browser!")
-                return None
-        webbrowser.get(chrome_path).open("./mymap.html")
+    '''
+    Function opens map webpage in the appropriate browser
+    '''
+    import platform
+    system = platform.system()
+    if system == "Darwin":
+        chrome_path = r'open -a /Applications/Google\ Chrome.app %s'
+    elif system == "win32":
+        # Windows
+        chrome_path = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
+    else:
+        print("Please open ./mymap.html in your favorite browser!")
+        return None
+    webbrowser.get(chrome_path).open("./mymap.html")
 
 def plot_pnts(coord_list):
-        latitudes  = [coord[0] for coord in coord_list]
-        longitudes = [coord[1] for coord in coord_list]
-        gmap = gmplot.GoogleMapPlotter(0,0,3)
-        gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/" 
-        gmap.scatter(latitudes, longitudes, 'k', marker=True)
-        #gmap.heatmap(latitudes, longitudes)
-        gmap.draw("mymap.html")
-        start_browser()
+    '''
+    Function takes a list of coordinates and plots them on
+    google maps plot.  Can either be a scatter plot or heatmap
+    '''
+    latitudes = [coord[0] for coord in coord_list]
+    longitudes = [coord[1] for coord in coord_list]
+    gmap = gmplot.GoogleMapPlotter(0, 0, 3)
+    gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
+    gmap.scatter(latitudes, longitudes, 'k', marker=True)
+    #gmap.heatmap(latitudes, longitudes)
+    gmap.draw("mymap.html")
+    start_browser()
 
 def main():
-        parser = argparse.ArgumentParser(description="input file")
-        parser.add_argument("-i", dest="filename", required=True, 
-                            help="The log file to be mapped", metavar="FILE",
-                            type=lambda x:is_valid_file(parser, x))
-        args = parser.parse_args()
+    '''
+    Main function, takes user supplied fail2ban log file and
+    converts it to map image
+    '''
+    parser = argparse.ArgumentParser(description="input file")
+    parser.add_argument("-i", dest="filename", required=True,
+                        help="The log file to be mapped", metavar="FILE",
+                        type=lambda x: is_valid_file(parser, x))
+    args = parser.parse_args()
 
-        while True:
-                try:
-                        coords = [map_ip(line.split()[-1]) for line in args.filename]
-                        for coord in coords:
-                                print(coord)
-                        plot_pnts(coords)
-                        break
-                except KeyboardInterrupt:
-                        print("Goodbye...")
-                        sys.exit(-1)
+    while True:
+        try:
+            coords = [map_ip(line.split()[-1]) for line in args.filename]
+            for coord in coords:
+                print(coord)
+            plot_pnts(coords)
+            break
+        except KeyboardInterrupt:
+            print("Goodbye...")
+            sys.exit(-1)
 
 if __name__ == "__main__":
-        main()
-
+    main()
